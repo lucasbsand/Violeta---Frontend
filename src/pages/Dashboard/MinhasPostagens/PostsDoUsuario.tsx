5b9c6eeb47cd3565b35fecd15f7f2f03ef0e623a
lucasbsand <124009743+lucasbsand@users.noreply.github.com>
2023-09-05 13:11:52 -0300

    Alteração minhas postagens (de novo)

--- src/pages/Dashboard/MinhasPostagens/PostsDoUsuario.tsx
+++ src/pages/Dashboard/MinhasPostagens/PostsDoUsuario.tsx
@@ -1,65 +1,95 @@
-import { Logo } from '../../Home/PrimeiraParte/HomeStyle'
-import { ButtonCriarPostagens, Container, Header, Image_Custom, LinePost, Links, TextPost, TitlePost, UsuarioPerfil } from './PostsStyle'
-import { Card_True_Content, Card_NullContent, IdeiaImagem } from './PostsStyle'
-import { Line } from './PostsStyle'
-import { Posts } from './Data/PostsData'
-import LogoImage from '../../../assets/ghost 1.svg'
-import Logo_Link_Perfil from '../MinhasPostagens/assets/profile 1.svg'
-import LineImage from './assets/Line 4.svg'
-import IdeiaImage from './assets/thought-bubble-svgrepo-com 1.svg'
-import Footer from '../../../components/Componentes pertencentes ao Login/Footer'
+import * as S from "./PostsStyle";
+import { Card_True_Content, Card_NullContent, IdeiaImagem } from "./PostsStyle";
+import { Line } from "./PostsStyle";
+import { Posts } from "./Data/PostsData";
+import LogoImage from "../../../assets/ghost 1.svg";
+import Logo_Link_Perfil from "../MinhasPostagens/assets/profile 1.svg";
+import LineImage from "./assets/Line 4.svg";
+import IdeiaImage from "./assets/thought-bubble-svgrepo-com 1.svg";
+import Footer from "../../../components/Componentes pertencentes ao Login/Footer";
+import Heart from "./assets/second_heart-svgrepo-com 1.svg";
+import Comment from "./assets/comment-3-svgrepo-com 1.svg";
 
 function PostsUsuario() {
-    return (
-        <>
-            <Container>
-                <Header>
-                    <Logo>
-                        <img src={LogoImage} />
-                    </Logo>
+  return (
+    <>
+      <S.Header>
+        <img src={LogoImage} />
 
-                    <ul>
-                        <li><Links to="#">Minhas Postagens</Links></li>
-                        <Line>
-                            <img src={LineImage} />
-                        </Line>
-                        <li><Links to="#">Outras Postagens</Links></li>
-                    </ul>
+        <ul>
+          <li>
+            <S.Links to="#">Minhas Postagens</S.Links>
+          </li>
+          <Line>
+            <img src={LineImage} />
+          </Line>
+          <li>
+            <S.Links to="#">Outras Postagens</S.Links>
+          </li>
+        </ul>
 
-                    <UsuarioPerfil to="#"><img src={Logo_Link_Perfil} /></UsuarioPerfil>
-                </Header>
+        <S.UsuarioPerfil to="#">
+          <img src={Logo_Link_Perfil} />
+        </S.UsuarioPerfil>
+      </S.Header>
 
-                <main>
-                    <Container>
-                        {Posts.length > 0 ? Posts.map((post, index) => (
-                            <Card_True_Content key={index}>
-                                <TitlePost>
-                                    {post.title}
-                                </TitlePost>
-                                <LinePost />
-                                <TextPost>
-                                    {post.text}
-                                </TextPost>
-                                <Image_Custom>
-                                    <img src={post.img} />
-                                </Image_Custom>
-                            </Card_True_Content>))
-                            :
-                            <Card_NullContent>
-                                <IdeiaImagem>
-                                    <img src={IdeiaImage} />
-                                </IdeiaImagem>
-                                <p>Ainda sem postagens...</p>
-                                <ButtonCriarPostagens>
-                                    CRIAR POSTAGEM
-                                </ButtonCriarPostagens>
-                            </Card_NullContent>}
-                    </Container>
-                </main>
-            </Container>
-            {Posts.length > 0 ? "" : <Footer />}
-        </>
-    )
+      <S.Main>
+        {Posts.length == 0 ? (
+          <Card_NullContent>
+            <IdeiaImagem>
+              <img src={IdeiaImage} />
+            </IdeiaImagem>
+            <p>Ainda sem postagens...</p>
+            <S.ButtonCriarPostagens>CRIAR POSTAGEM</S.ButtonCriarPostagens>
+          </Card_NullContent>
+        ) : (
+          <S.Container>
+            {Posts.map((post, index) => (
+              <Card_True_Content key={index}>
+                <S.Image_Custom>
+                  <img src={post.img} />
+                </S.Image_Custom>
+                <S.TitleContainer>
+                  <S.TitlePost>{post.title}</S.TitlePost>
+                  <S.LinePost />
+                  <img src={Heart} alt="Coração" height="34px" />
+                  <img src={Comment} alt="Comentário" height="30px" />
+                </S.TitleContainer>
+                <S.TextPost>{post.text.length > 244 ? post.text.slice(0, 244) + " ..." : post.text}</S.TextPost>
+              </Card_True_Content>
+            ))}
+          </S.Container>
+        )}
+      </S.Main>
+      {console.log(Posts[0].text.length)}
+      {Posts.length > 0 ? "" : <Footer />}
+    </>
+  );
 }
 
-export default PostsUsuario
+{
+  /* <S.Container>
+  {Posts.length > 0 ? (
+    Posts.map((post, index) => (
+      <Card_True_Content key={index}>
+        <S.TitlePost>{post.title}</S.TitlePost>
+        <S.LinePost />
+        <S.TextPost>{post.text}</S.TextPost>
+        <S.Image_Custom>
+          <img src={post.img} />
+        </S.Image_Custom>
+      </Card_True_Content>
+    ))
+  ) : (
+    <Card_NullContent>
+      <IdeiaImagem>
+        <img src={IdeiaImage} />
+      </IdeiaImagem>
+      <p>Ainda sem postagens...</p>
+      <S.ButtonCriarPostagens>CRIAR POSTAGEM</S.ButtonCriarPostagens>
+    </Card_NullContent>
+  )}
+</S.Container>; */
+}
+
+export default PostsUsuario;
--- src/pages/Dashboard/MinhasPostagens/PostsStyle.ts
+++ src/pages/Dashboard/MinhasPostagens/PostsStyle.ts
@@ -1,142 +1,204 @@
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Main = styled.main`
+  margin-top: 5rem;
+`;
 
 export const Container = styled.div`
- margin: 0 auto;
- margin-top: 2rem;
- margin-bottom: 13.75rem;
- width: 90%;
-`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  margin: 0 auto;
+  margin-bottom: 13.75rem;
+  padding-right: 34px;
+  gap: 24px 51px;
+  width: 900px;
+  height: 747px;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 36px; /* width of the entire scrollbar */
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #444444; /* color of the tracking area */
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #D9D9D980; /* color of the scroll thumb */ /* roundness of the scroll thumb */
+    border: 1px solid #444 /* creates padding around scroll thumb */
+  }
+`;
 
 export const UsuarioPerfil = styled(Link)`
- img {
-  width: 90px;
-  height: 85px;
-  flex-shrink: 0;
- }
-`
+  img {
+    width: 90px;
+    height: 85px;
+    flex-shrink: 0;
+  }
+`;
 
 export const Card_NullContent = styled.div`
- display: flex;
- justify-content: center;
- flex-direction: column;
- align-items: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 
- img {
-  
- }
+  img {
+  }
 
- p {
-  color: #FFF;
-  opacity: 0.5;
+  p {
+    color: #fff;
+    opacity: 0.5;
+    text-align: center;
+    font-family: Esteban;
+    font-size: 45px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: 3.825px;
+  }
+`;
+
+export const IdeiaImagem = styled.div`
+  img {
+    width: 220px;
+    height: 220px;
+    flex-shrink: 0;
+  }
+`;
+
+export const ButtonCriarPostagens = styled.button`
+  color: #000;
+  width: 251px;
+  height: 69px;
+  flex-shrink: 0;
+  cursor: pointer;
+  margin: 20px;
+
   text-align: center;
   font-family: Esteban;
-  font-size: 45px;
+  font-size: 20px;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
-  letter-spacing: 3.825px;
- }
-`
+  letter-spacing: 1.7px;
 
-export const IdeiaImagem = styled.div`
- img {
-   width: 220px;
-   height: 220px;
-   flex-shrink: 0;
- }
-`
-
-export const ButtonCriarPostagens = styled.button`
- color: #000;
- width: 251px;
- height: 69px;
- flex-shrink: 0;
- cursor: pointer;
- margin: 20px;
-
- text-align: center;
- font-family: Esteban;
- font-size: 20px;
- font-style: normal;
- font-weight: 400;
- line-height: normal;
- letter-spacing: 1.7px;
-
- border-radius: 50px;
- border: 1px solid #000;
- background: #F1D261;
-`
+  border-radius: 50px;
+  border: 1px solid #000;
+  background: #f1d261;
+`;
 
 export const Line = styled.p`
- width: 54.781px;
- height: 0px;
- background: #FFF;
- margin-left: 50px;
- margin-right: 14px;
- margin-top: 8px;
-`
+  width: 54.781px;
+  height: 0px;
+  background: #fff;
+  margin-left: 50px;
+  margin-right: 14px;
+  margin-top: 8px;
+`;
 
 export const Header = styled.header`
- display: flex;
- justify-content: space-between;
- margin-top: -20px;
- li{
+  display: flex;
+  align-items: center;
+  width: 90%;
+  justify-content: space-between;
+  margin: 30px auto 0;
+  li {
     list-style: none;
     margin-top: 20px;
- }
+  }
 
- ul {
+  ul {
     display: flex;
- }
-`
+  }
+`;
 
 export const Links = styled(Link)`
- text-decoration: none;
- color: #FFF;
- font-family: Esteban;
- font-size: 20px;
- font-style: normal;
- font-weight: 400;
- line-height: normal;
- letter-spacing: 1.7px;
-`
+  text-decoration: none;
+  color: #fff;
+  font-family: Esteban;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 1.7px;
+`;
 
 // Components do post
 export const Card_True_Content = styled.div`
- width: 880px;
- height: 490px;
- margin: 0 auto;
- background: #444;
- flex-shrink: 0;
- padding: 20px;
-`
+  width: 383px;
+  height: 481px;
+  margin: 0 auto;
+  background: #444;
+  flex-shrink: 0;
+  transition: .3s;
 
-export const TitlePost = styled.h1`
+  &:hover {
+   cursor: pointer;
+   scale: .98;
+  }
+`;
+
+export const TitlePost = styled.p`
   @font-face {
     font-family: Haettenschweiler;
-    src: url('/Haettenschweiler Regular.ttf');
- }
+    src: url("/Haettenschweiler Regular.ttf");
+  }
 
- color: #FFF;
- text-align: start;
- font-family: Haettenschweiler;
- font-size: 50px;
- font-style: normal;
- font-weight: 400;
- line-height: normal;
- letter-spacing: 4.25px;
-`
+  color: #fff;
+  text-align: center;
+  font-family: Haettenschweiler;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 2.72px;
+`;
+
+export const TitleContainer = styled.div`
+  height: 34px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  column-gap: 10px;
+  margin: 15px 19px;
+`;
 
 export const TextPost = styled.p`
- 
-`
+  color: #fff;
+  font-family: Esteban;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 1.275px;
+  margin: 0 20px 24px;
+`;
+
+export const MoreText = styled(TextPost)`
+   text-decoration: underline;
+   font-weight: bold;
+`;
 
 export const Image_Custom = styled.div`
- 
-`
+  width: 383px;
+  height: 273px;
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+
+  & img {
+    object-fit: cover;
+    height: fit-content;
+    height: 382px;
+  }
+`;
 
 export const LinePost = styled.hr`
- width: 100%;
-`
-    
+  align-self: center;
+  height: 30px;
+  width: 3px;
+  background-color: #ffffff;
+`;
